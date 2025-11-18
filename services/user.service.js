e97bfc1d4280fb5c533ab9a6266724e85fdab57c
Goatz <Goataragua@gmail.com>
2025-11-18 19:52:03 +0200

    Create user services and controllers

---
+++ services/user.service.js
@@ -0,0 +1,97 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+// Create a user
+async function createUser(userData) {
+    const { username, email, password } = userData;
+    
+    // Check if email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new Error("Email already in use");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    return user;
+}
+
+// Find a user by username
+async function getUserByUsername(username) {
+    const result = await User.findOne({ username });
+    return result;
+}
+
+// Find user by email
+async function getUserByEmail(email) {
+    const user = await User.findOne(email);
+    return user;
+}
+
+// Validate user credentials
+async function validateUserCredentials(email, password){
+    const user = await User.findOne(email);
+    if(!user){
+        throw new Error("Invalid email or password");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new Error("Invalid email or password");
+    }
+
+    return user;
+}
+
+// Find a user by Id
+async function getUserById(userId){
+    const user = await User.findById(userId).select("-password");
+    if(!user) {
+        throw new Error("User not found");
+    }
+    return user;
+}
+
+async function updateUser(userId, updateData){
+    const user = await User.findbyIdAndUpdate(userId, updateData, {
+        new: true,
+    }).select("-password");
+
+    if(!user){
+        throw new Error("User not found");
+    }
+
+    return user;
+}
+
+async function deleteUser(userId){
+    const user = await User.findByIdAndDelete(userId);
+    if(!user){
+        throw new Error("User not found");
+    }
+    return user;
+}
+
+// Find all users
+async function findAll() {
+    const result = await User.find();
+    return result;
+}
+
+module.exports = {
+    createUser,
+    getUserByUsername,
+    getUserById,
+    getUserByEmail,
+    updateUser,
+    deleteUser,
+    findAll
+}
