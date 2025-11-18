af8fb6375f56d499491945bca63480b8b7b3a2dc
Goatz <Goataragua@gmail.com>
2025-11-18 19:54:32 +0200

    Create user services and controllers

---
+++ controllers/user.controller.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+const userService = require('../services/user.service');
+
+// Create User
+exports.createUserController = async(req, res) => {
+    try {
+        const result = await userService.createUser(req.body);
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Get user by username
+exports.getUserByUsernameController = async (req, res) => {
+    try {
+        const user = await userService.getUserByUsername(req.params.username);
+        if(!user) return res.status(404).json({ error: "User not found"});
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+// Get user by id
+exports.getUserByIdController = async(req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        if(!user) return res.status(404).json({ error: "User not found"});
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
