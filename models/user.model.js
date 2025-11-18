55c047c2aa9c0da44646488a7c5b07edd1108ba4
Goatz <Goataragua@gmail.com>
2025-11-18 18:00:23 +0200

    Add user.model

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        watchlist: [
+            {
+                movieId: String,
+                title: String,
+                poster: String,
+                addedAt: { type: Date, default: Date.now}
+            }
+        ],
+    },
+    { timestamps: true}
+)
+
+export default mongoose.model("User", userSchema);
