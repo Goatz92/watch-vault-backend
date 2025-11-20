ecdbdedc018048c4e942f72767d2eb1ef03a80be
Goatz <Goataragua@gmail.com>
2025-11-20 16:12:43 +0200

    Remove redundant route names. Add comments

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,20 +3,35 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 
-// User handling routes
-// Post routes
+/**
+ * USER ROUTES
+ * Base path: /api/users
+ */
+
+// Create a new user (Register)
+
 router.post('/register', userController.createUserController);
 
-// Get routes
-router.get('/findUser/:username', userController.getUserByUsernameController);
-router.get('/findEmail/:email', userController.getUserByEmailController);
-router.get('/findId/:id', userController.getUserByIdController);
-router.get('/findAll', userController.getAllUsersController);
+/**
+ * GET Routes
+ * - Get user by username
+ * - Get user by email
+ * - Get user by ID
+ * - Get all users
+ */
+router.get('/username/:username', userController.getUserByUsernameController);
+router.get('/email/:email', userController.getUserByEmailController);
+router.get('/:id', userController.getUserByIdController);
+router.get('/', userController.getAllUsersController);
 
-// Patch routes
+/**
+ * Update user
+ */
 router.patch('/updateUser/:id', userController.updateUserController);
 
-// Delete routes
+/**
+ * Delete user
+ */
 router.delete('/deleteUser/:id', userController.deleteUserController);
 
 module.exports = router;
