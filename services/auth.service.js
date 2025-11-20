01d6b37420d4e3daa18b078ca5fe7076b2db8829
Goatz <Goataragua@gmail.com>
2025-11-20 16:46:51 +0200

    Create auth services

---
+++ services/auth.service.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+// Generate JWT
+function generateToken(user) {
+    return jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
+}
+
+// Hash password
+async function hashPassword (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+async function comparePasswords (rawPassword, hashedPassword) {
+    return await bcrypt.compare(rawPassword, hashedPassword);
+}
+
+// Login with email + password
+async function loginUser(email, password) {
+    // Find user by email
+    const user = await comparePasswords(password, user.password);
+    if (!isMatch) {
+        throw new Error("Invalid email or password");
+    }
+
+    // Generate token
+    const token = generateToken(user);
+
+    const safeUser = {
+        id: user._id,
+        username: user.username,
+        email: user.email
+    };
+
+    return { user: safeUser, token };
+}
+
+// Verify JWT 
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        throw new Error("Invalid or expired token");
+    }
+}
+
+module.exports = {
+    loginUser,
+    hashPassword,
+    comparePasswords,
+    generateToken,
+    verifyToken
+};
