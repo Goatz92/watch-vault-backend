6e730aaf7fb290d6dc29f94d59744db284edc3f5
Goatz <Goataragua@gmail.com>
2025-11-21 18:43:59 +0200

    Feat: updated routes to use auth middleware

--- routes/watchlist.routes.js
+++ routes/watchlist.routes.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
+
+// Controllers
 const watchlistController = require('../controllers/watchlist.controller');
 
+// Middleware
+const authenticateToken = require('../middleware/auth.middleware');
+
 // Add movie
-router.post("/:userId/add", watchlistController.addToWatchlistController);
+router.post("/:userId/add", authenticateToken, watchlistController.addToWatchlistController);
 
 // Remove movie
-router.delete("/:userId/remove/:imdbID", watchlistController.removeFromWatchlistController);
+router.delete("/:userId/remove/:imdbID", authenticateToken, watchlistController.removeFromWatchlistController);
 
 // Get all movies in watchlist
-router.get("/:userId", watchlistController.getUserWatchlistController);
+router.get("/:userId", authenticateToken, watchlistController.getUserWatchlistController);
 
 // Full watchlist clear
-router.delete("/:userId/clear", watchlistController.clearWatchlistController);
+router.delete("/:userId/clear", authenticateToken, watchlistController.clearWatchlistController);
 
 module.exports = router;
