cd22dd0289ec48589abbfe4264b7945a8b7cd19d
Goatz <Goataragua@gmail.com>
2025-11-21 17:44:44 +0200

    Create watchlist controllers

---
+++ controllers/watchlist.controller.js
@@ -0,0 +1,44 @@
+const watchlistService = require('../services/watchlist.service');
+
+exports.addToWatchlistController = async (req, res) => {
+    try {
+        const { imdbID } = req.body;
+        const userId = req.params.userId;
+
+        if (!imdbID) {
+            res.status(400).json({ error: "IMDb ID is required"});
+        }
+
+        const item = await watchlistService.addToWatchlist(userId, imdbID);
+        res.status(201).json(item);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.removeFromWatchlistController = async (req, res) => {
+    try {
+        const { imdbID } = req.params;
+        const userId = req.params.userId;
+
+        const removed = await watchlistService.removeFromWatchlist(userId, imdbID);
+        res.status(200).json({ message: "Removed from watchlist", removed });
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+};
+
+exports.clearWatchlistController = async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const result = await watchlistService.clearWatchlist(userId);
+
+        res.status(200).json({
+            message: "Watchlist cleared",
+            deleteCount: result.deletedCount
+        });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
