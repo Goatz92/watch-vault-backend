4a3bd19825ec361e400694310a8abefccdc0ec0c
Goatz <Goataragua@gmail.com>
2025-11-21 16:43:51 +0200

    Add warnings to required fields

--- models/user.model.js
+++ models/user.model.js
@@ -4,19 +4,19 @@ const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
-            required: true,
+            required: [true, 'Please provide a Username'],
             unique: true,
             trim: true
         },
         email: {
             type: String,
-            required: true,
-            unique: true,
+            required: [true, 'Please provide an Email'],
+            unique: [true, 'Email already exists'],
             lowercase: true
         },
         password: {
             type: String,
-            required: true
+            required: [true, 'Please provide a password']
         },
         watchlist: [
             {
