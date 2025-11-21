0df4439290a4a9556995e7136d43a2bcbf9a1982
Goatz <Goataragua@gmail.com>
2025-11-21 17:51:35 +0200

    Add watchlist routing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+// Routes
 const userRoutes = require('./routes/user.routes');
 const authRoutes = require('./routes/auth.routes');
-const movieRoutes = require('./routes/movie.routes')
+const movieRoutes = require('./routes/movie.routes');
+const watchlistRoutes = require('./routes/watchlist.routes');
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
+app.use('/api/watchlist', watchlistRoutes);
 
 // Default route
 app.get('/', (req, res) => {
