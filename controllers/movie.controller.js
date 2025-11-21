5fa6a8c136343c4400b4c68bff99386da94beaaa
Goatz <Goataragua@gmail.com>
2025-11-21 17:16:46 +0200

    Create movie controllers to handle API calls

---
+++ controllers/movie.controller.js
@@ -0,0 +1,32 @@
+const movieService = require('../services/movie.service');
+
+// Search movies by title
+exports.searchMoviesController = async (req, res) => {
+    try {
+        const { title } = req.query;
+        if (!title) {
+            return res.status(400).json({ error: "Title is required" });
+        }
+
+        const movies = await movieService.searchMovies(title);
+        res.status(200).json(movies);
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+};
+
+exports.getMovieByIdController = async (req, res) => {
+    try {
+        const { imdbID } = req.params;
+
+        if (!imdbID) {
+            return res.status(400).json({ error: "Imdb ID is required" });
+        }
+
+        const movie = await movieService.getMovieById(imdbID);
+        res.status(200).json(movie);
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+};
+
