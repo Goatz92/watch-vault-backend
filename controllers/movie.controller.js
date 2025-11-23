a40f082b3deb5054ae2b2d90b132a9b54bcbfb10
Goatz <Goataragua@gmail.com>
2025-11-23 18:19:24 +0200

    Update movieService name to omdbApiService

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -3,7 +3,7 @@
  * Handles incoming HTTP requests related to movie search and movie details.
  */
 
-const movieService = require('../services/movie.service');
+const omdbApiService = require('../services/omdbApi.service');
 
 /**
  * @desc    Search movies by title
@@ -17,7 +17,7 @@ exports.searchMoviesController = async (req, res) => {
             return res.status(400).json({ error: "Title is required" });
         }
 
-        const movies = await movieService.searchMovies(title);
+        const movies = await omdbApiService.searchMovies(title);
         res.status(200).json(movies);
     } catch (err) {
         res.status(404).json({ error: err.message });
@@ -37,7 +37,7 @@ exports.getMovieByIdController = async (req, res) => {
             return res.status(400).json({ error: "Imdb ID is required" });
         }
 
-        const movie = await movieService.getMovieById(imdbID);
+        const movie = await omdbApiService.getMovieById(imdbID);
         res.status(200).json(movie);
     } catch (err) {
         res.status(404).json({ error: err.message });
